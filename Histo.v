450785b8ef9fe85c7069ce2e51987576126b97d7
Thomas James Merry <tjmer2@AD.MONASH.EDU>
2016-05-23 15:58:03 +1000

    Signed-off-by: Thomas James Merry <tjmer2@AD.MONASH.EDU>

--- Histo.v
+++ Histo.v
@@ -11,15 +11,17 @@
 //His_gry
 
 
-module(
+module Histo(
 input iPclk,
 input [15:0] iY_cont,
 input [15:0] iX_cont,
 input	Dval,
 input Fval,
 input [7:0] Grey,
-output [7:0] Gr_Out;
-)
+output reg [7:0] Gr_Out_His,
+output reg [7:0] Gr_Out_Cum,
+output [1:0] stateOut
+);
 
 wire [11:0] SRAM_W_Addr_In;
 wire [19:0] SRAM_D_In;
@@ -46,7 +48,7 @@ reg writeEnable;
 reg [7:0] PixCount;
 reg [19:0] CumVal;
 
-HisRam SRAM(
+SRAM HisRam(
 	.clock(iPclk),
 	.data(SRAM_D_In),
 	.rdaddress(SRAM_R_Addr_In),
@@ -54,7 +56,7 @@ HisRam SRAM(
 	.wren(SRAM_Wen),
 	.q(SRAM_Q_Out));
 	
-DispRam SRAM(
+SRAM DispRam(
 	.clock(iPclk),
 	.data(DISP_SRAM_D_In),
 	.rdaddress(DISP_SRAM_R_Addr_In),
@@ -62,7 +64,7 @@ DispRam SRAM(
 	.wren(DISP_SRAM_Wen),
 	.q(DISP_SRAM_Q_Out));
 	
-HDispRam SRAM(
+SRAM HDispRam(
 	.clock(iPclk),
 	.data(CUM_SRAM_D_In),
 	.rdaddress(CUM_SRAM_R_Addr_In),
@@ -78,56 +80,61 @@ always@ (posedge iPclk) begin
 	PixCount <= 0;
 	end
 	else begin
+	
 		case (state)
 		
 		0: state <= 1;
 		
 		1:	begin //Clear the Memory
-				PixCount = PixCount + 1;
+				PixCount <= PixCount + 1;
 				if(PixCount >= 255) begin
-					state = 2;
-					PixCount = 0;
+					state <= 2;
+					PixCount <= 0;
 				end
 			end
 		2: begin //Copy the DISPLAY HISTOGRAM and CALC the CUM
-				PixCount = PixCount + 1;
+				PixCount <= PixCount + 1;
 				CumVal = SRAM_Q_Out + CumVal;
 				if(PixCount >= 255) begin
-					state = 3;
-					PixCount = 0;
-					CumVal = 0;
+					state <= 3;
+					PixCount <= 0;
+					CumVal <= 0;
 				end
 			end
 		3: begin	//CLEAR THE NORMAL HISTOGRAM
-				PixCount = PixCount + 1;
-				end
+				PixCount <= PixCount + 1;
 			end
-		default: state = 3;
-	
+		default: state <= 3;
+	endcase
 	end
-	wireEnable <= Fval;
-	addrHolding <= gry;
-	holding <= SRAM_Q_Out;
 	
+	
+	addrHolding <= Grey;
+	holding <= SRAM_Q_Out+1;
+	
+	
+	Gr_Out_His <= (DISP_SRAM_Q_Out > iX_cont)?8'b11111111:8'b0;
+	Gr_Out_Cum <= (CUM_SRAM_Q_Out > iX_cont)?8'b11111111:8'b0;
 	
 	
 end
 assign SRAM_R_Addr_In[11:0] =				state[0]?(state[1]? /*1*/0 : /*3*/0) 			:(state[1]? /*0*/ {4'b0000,Grey}: /*2*/PixCount);
 assign SRAM_D_In[19:0] = 					state[0]?(state[1]? /*1*/0 : /*3*/0) 			:(state[1]? /*0*/holding :			 /*2*/0);
-assign SRAM_W_Addr_In[11:0] = 			state[0]?(state[1]? /*1*/0 : /*3*/PixCount) 	:(state[1]? /*0*/AddrHolding:		 /*2*/0);
+assign SRAM_W_Addr_In[11:0] = 			state[0]?(state[1]? /*1*/0 : /*3*/PixCount) 	:(state[1]? /*0*/addrHolding:		 /*2*/0);
 assign SRAM_Wen = 							state[0]?(state[1]? /*1*/0 : /*3*/1) 			:(state[1]? /*0*/ 1:					 /*2*/0);
 
-assign DISP_SRAM_R_Addr_In[11:0] = 		state[0]?(state[1]? /*1*/0 : /*3*/0) 			:(state[1]? /*0*/0+: /*2*/0);
+assign DISP_SRAM_R_Addr_In[11:0] = 		state[0]?(state[1]? /*1*/0 : /*3*/0) 			:(state[1]? /*0*/iY_cont[7:0]: /*2*/0);
 assign DISP_SRAM_D_In[19:0] = 			state[0]?(state[1]? /*1*/0 : /*3*/0) 			:(state[1]? /*0*/0: /*2*/SRAM_Q_Out);
 assign DISP_SRAM_W_Addr_In[11:0] = 		state[0]?(state[1]? /*1*/PixCount : /*3*/0) 	:(state[1]? /*0*/0: /*2*/PixCount);
 assign DISP_SRAM_Wen = 						state[0]?(state[1]? /*1*/1 : /*3*/0) 			:(state[1]? /*0*/0: /*2*/1);
 
 
-assign CUM_SRAM_R_Addr_In[11:0] = 		state[0]?(state[1]? /*1*/0 : /*3*/0) 			:(state[1]? /*0*/0: /*2*/0);
+assign CUM_SRAM_R_Addr_In[11:0] = 		state[0]?(state[1]? /*1*/0 : /*3*/0) 			:(state[1]? /*0*/iY_cont[7:0]: /*2*/0);
 assign CUM_SRAM_D_In[19:0] = 				state[0]?(state[1]? /*1*/0 : /*3*/0) 			:(state[1]? /*0*/0: /*2*/CumVal);
 assign CUM_SRAM_W_Addr_In[11:0] = 		state[0]?(state[1]? /*1*/PixCount : /*3*/0) 	:(state[1]? /*0*/0: /*2*/PixCount);
 assign CUM_SRAM_Wen = 						state[0]?(state[1]? /*1*/1 : /*3*/0) 			:(state[1]? /*0*/0: /*2*/1);
 
 
+assign stateOut = state;
 
 endmodule
